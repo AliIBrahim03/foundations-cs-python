6eb8f10db354c6b6be8c78f19c67399e4c9d530a
Ali <ali.ibrahim1552@gmail.com>
2023-10-29 16:07:26 +0200

    Added conditions and fixed the name of a function

--- Assignment3.py
+++ Assignment3.py
@@ -2,7 +2,7 @@ def addMatrices():
     pass
 def checkRotation():    
     pass
-def incertDictionary():
+def invertDictionary():
     pass
 def convert_Matrix_D():
     pass
@@ -12,7 +12,7 @@ def search():
     pass
 def displayMenu(): #displaying the options we have
 
-    print("1. Add Matrices\n" + "2. Check Rotation\n" + "3. Invert Dictionary\n"
+    print("\n1. Add Matrices\n" + "2. Check Rotation\n" + "3. Invert Dictionary\n"
           + "4. Convert Matrix to Dictionary\n" + "5. Check Palindrome\n" + 
           "6. Search for an Element & Merge Sort\n" + "7. Exit")
     
@@ -24,7 +24,27 @@ def main(): #main function
     
     print("Welcome", name) 
     
-    displayMenu() 
+    choice = 10
+    limit = 0
+    while choice != 7 and limit < 7 :
+        displayMenu() 
+        choice = eval(input("\nEnter your choice: "))
+        if choice == 1:
+            addMatrices()
+        elif choice == 2:
+            checkRotation()
+        elif choice == 3:
+            invertDictionary()
+        elif choice == 4:
+            convert_Matrix_D()
+        elif choice == 5:
+            checkPalindrome()
+        elif choice == 6:
+            search()
+        elif choice != 7:
+            print("\nInvalid, try again")
+    
+    
     
     
 main()
