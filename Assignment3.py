58db56f61e463bfa3d8463b3a3bb357f23875a88
Ali <ali.ibrahim1552@gmail.com>
2023-10-29 15:37:30 +0200

    Added the menu

--- Assignment3.py
+++ Assignment3.py
@@ -1,7 +1,30 @@
+def addMatrices():
+    pass
+def checkRotation():    
+    pass
+def incertDictionary():
+    pass
+def convert_Matrix_D():
+    pass
+def checkPalindrome():
+    pass
+def search():
+    pass
+def displayMenu(): #displaying the options we have
+
+    print("1. Add Matrices\n" + "2. Check Rotation\n" + "3. Invert Dictionary\n"
+          + "4. Convert Matrix to Dictionary\n" + "5. Check Palindrome\n" + 
+          "6. Search for an Element & Merge Sort\n" + "7. Exit")
+    
+    
+    
 def main(): #main function
-    name = input(print("Please enter your name: "))  
+
+    name = input(print("Please enter your name: "))
+    
     print("Welcome", name) 
     
+    displayMenu() 
     
     
 main()
