0b00bdc15976f6071eb3568cef797c29245647d0
Ali <ali.ibrahim1552@gmail.com>
2023-10-30 17:54:50 +0200

    rotate function added

--- Assignment3.py
+++ Assignment3.py
@@ -29,14 +29,46 @@ def addMatrices():
         matrix3.append(row3) 
     print("Matrix 3:",matrix3)
             
-    return matrix3
-       
+    return matrix3     
             
-        
+def checkRotation():  
+   rows1 = int(input("Enter the number of rows of the 1st matrix: "))
+   columns1 = int(input("Enter the number of columns of the first matrix: "))
+   matrix1 = []
+   for i in range(rows1):
+       row = []
+       for j in range(columns1):
+           element = int(input("Enter element " + str(j+1) + " of row " + str(i+1) + " of the first matrix: "))
+           row.append(element)
+       matrix1.append(row)
+       print(matrix1)
+   
+   rows2 = int(input("Enter the number of rows of 2nd matrix: "))
+   columns2 = int(input("Enter the number of columns of 2nd matrix: "))
+   matrix2 = []
+   for i in range(rows2):
+       row = []
+       for j in range(columns2):
+           element = int(input("Enter element " + str(j+1) + " of row " + str(i+1) + " of the second matrix: "))
+           row.append(element)
+       matrix2.append(row)
+       print(matrix2)
+   
+
+   total_elements_in_matrix1 = sum(len(sublist) for sublist in matrix1)
+   total_elements_in_matrix2 = sum(len(sublist) for sublist in matrix2)
     
-    
-def checkRotation():    
-    pass
+   if  total_elements_in_matrix1 != total_elements_in_matrix2 :
+       print("These matrices are not rotated")
+       
+       for i in range(len(matrix1)):
+           for j in range(len(matrix1[0])):
+               if matrix1[i][j] != matrix2[j][i]:
+                   print("These matrices are not rotated")
+   else:
+    print("These matrices are rotated")                
+               
+               
 def invertDictionary():
     lenght = int(input("Please enter the number of elements of this dictionary: "))
     dict = {}
@@ -56,10 +88,11 @@ def invertDictionary():
     print(inverted_dict)        
     
 def convert_Matrix_D():
+    
     pass
-def checkPalindrome(s): #here I assumed the user will input at least one character   
-#base case 
-    if len(s) <= 1:
+def checkPalindrome(s): #I assumed the user will input at least one character   
+ 
+    if len(s) <= 1: #Base Case
         return True
     first_letter = s[0]
     last_letter = s[-1]
@@ -69,21 +102,25 @@ def checkPalindrome(s): #here I assumed the user will input at least one charact
         return False
     
 def search():
+    user_input = input("Enter a list of elements: ")
+    lst = user_input.split()  
+    element = input("Please enter the element to look for: ")
     pass
+    
 def displayMenu(): #displaying the options we have
 
     print("\n1. Add Matrices\n" + "2. Check Rotation\n" + "3. Invert Dictionary\n"
           + "4. Convert Matrix to Dictionary\n" + "5. Check Palindrome\n" + 
           "6. Search for an Element & Merge Sort\n" + "7. Exit")
     
-    
+def sort():
+    pass
     
 def main(): #main function
 
     name = input(print("Please enter your name: "))
     
     print("Welcome", name) 
-    
     choice = 10
     while choice != 7:
         displayMenu() 
@@ -92,6 +129,7 @@ def main(): #main function
             addMatrices()
         elif choice == 2:
             checkRotation()
+            
         elif choice == 3:
             invertDictionary()
         elif choice == 4:
