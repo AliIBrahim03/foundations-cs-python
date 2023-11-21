f2109fb8004e09f0fbcdef59f14e57dfc592c1ff
Ali <ali.ibrahim1552@gmail.com>
2023-11-21 20:27:44 +0200

    added content of displayMenu, getChoice and getUsername

--- Assignment 4.py
+++ Assignment 4.py
@@ -1,11 +1,30 @@
-def getChoice():
-    pass
-
 def getUsername():
-    pass   
+    while True:    
+        username = str(input("Please enter your name: ")).strip()
+        if username == '':
+            print("\nName cannot be empty. Please enter your name: ")
+        else:
+            return username
+            
+def getChoice():
+    choice = int(input("\nChoose an action: \n"))
+    return choice
+
+def displayMenu():
+    print("1. Singly Linked List"
+          +"\n2. Check if Palindrome"
+          +"\n3. Priority Queue"
+          +"\n4. Evaluate an Infix Expression"
+          +"\n5. Graph"
+          +"\n6. Exit")
 
 def main():
-    while (choice != 6):
+    
+    username = getUsername()
+    print("\nWelcome",username)
+    displayMenu()
+    choice = getChoice()
+    while choice != 6:
         
         if choice == 1:
             pass
@@ -17,7 +36,12 @@ def main():
             pass
         elif choice == 5:
             pass
-        elif choice != 6
+        elif choice != 6:
+            print("Please choose a valid action.\n")
+        displayMenu()
+        getChoice()
+            
+    print("You exited the program. Bye")
     
 main()    
 
