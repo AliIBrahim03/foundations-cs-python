12dd0327cead1543b23376133e9e24c1cfc79eac
Ali <ali.ibrahim1552@gmail.com>
2023-11-21 19:20:49 +0200

    added choices in main function

--- Assignment 4.py
+++ Assignment 4.py
@@ -5,8 +5,21 @@ def getUsername():
     pass   
 
 def main():
-    pass
-
+    while (choice != 6):
+        
+        if choice == 1:
+            pass
+        elif choice == 2 :
+            pass
+        elif choice == 3:
+            pass
+        elif choice == 4:
+            pass
+        elif choice == 5:
+            pass
+        elif choice != 6
+    
+main()    
 
     
     
